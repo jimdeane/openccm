d94b4a4e490e0044af319b62b5221aedf98d95ec
Jim Deane <jim@deane.com>
2020-07-14 12:19:41 +0100

    test

--- Application/aspnet-core-dotnet-core.UnitTests/SampleUnitTests.cs
+++ Application/aspnet-core-dotnet-core.UnitTests/SampleUnitTests.cs
@@ -11,7 +11,7 @@ namespace aspnet_core_dotnet_core.UnitTests
         public void IndexPageTest()
         {
             IndexModel model = new IndexModel();
-            Assert.AreEqual("Index11", actual: model.DoTest());
+            Assert.AreEqual("Index", actual: model.DoTest());
         }
 
     }
