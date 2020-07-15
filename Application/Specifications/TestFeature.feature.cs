6c5856a7cb4373ca8174c5d9a057288ca76b5105
Jim Deane <jim@deane.com>
2020-07-15 14:00:26 +0100

    changed test projects to include the word test and the pipeline to include them

--- Application/Specifications/TestFeature.feature.cs
+++ Application/Specifications/TestFeature.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace Specifications
+namespace SpecificationTests
 {
     using TechTalk.SpecFlow;
     using System;
